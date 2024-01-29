3ddb52b81fa36cfa0233389a34987719616382d0
Viktor Ufimtsev <buktor@bk.ru>
2024-01-29 20:12:26 +0700

    feat: fs implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs, {constants} from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+    fs.access('./src/fs/files/fresh.txt', constants.F_OK, (err)=>{
+        if (err) {
+            fs.writeFile(
+                './src/fs/files/fresh.txt',
+                'I am fresh and young',
+                'utf8',
+                (err) => {
+                    if (err) throw err
+                }       
+            );
+        }
+        else {
+            throw new Error('FS operation failed');
+        }
+    }) 
 };
 
 await create();
