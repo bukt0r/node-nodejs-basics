c80638ec35ac8a91e854c62f295c4255bd63060c
Viktor Ufimtsev <buktor@bk.ru>
2024-01-29 23:05:50 +0700

    feat: fs implement function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs, {constants} from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    fs.access('./src/fs/files/fileToRead.txt', constants.F_OK, (err)=>{
+        if (err) {
+            throw new Error('FS operation failed') 
+        } else {
+            fs.readFile('./src/fs/files/fileToRead.txt', 'utf8', (err, data) => {
+                if (err) throw err;
+            
+                console.log(data);
+            });
+        }
+    })
 };
 
 await read();
