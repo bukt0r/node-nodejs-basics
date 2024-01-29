d1762bf12387af4965d0cd2e6ae8deaf00287e48
Viktor Ufimtsev <buktor@bk.ru>
2024-01-29 23:04:43 +0700

    feat: fs implement function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs, {constants} from 'node:fs';
+
 const list = async () => {
-    // Write your code here 
+    fs.access('./src/fs/files', constants.F_OK, (err)=>{
+        if (err) {
+            throw new Error('FS operation failed') 
+        } else {
+            fs.readdir('./src/fs/files', 'utf8', (err, files) => {
+                if (err) throw err;
+            
+                console.log(files);
+            });
+        }
+    })
 };
 
 await list();
