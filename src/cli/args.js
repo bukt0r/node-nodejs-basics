6c4711ba6017236e5a101b9dfb38ec605b717a79
Viktor Ufimtsev <buktor@bk.ru>
2024-01-30 01:05:30 +0700

    feat: cli implement function args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arr = process.argv
+    const obj = {};
+
+    for (let i=0; i<arr.length; i +=2) {
+        const key = arr[i];
+        const value = arr[i+1];
+
+        obj[key] = value
+    }
+    console.log(Object.keys(obj)
+        .filter((key) => key.startsWith('--'))
+        .map((key) => `${key} is ${obj[key]}`)
+        .join(', ')
+        .replace(/--/g, '')
+        
+    );
 };
 
 parseArgs();
