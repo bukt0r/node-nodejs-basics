91f5557fd262dca46c963a98c7bc419d3ca206fa
Viktor Ufimtsev <buktor@bk.ru>
2024-01-29 21:13:23 +0700

    feat: fs implement function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+    fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true, errorOnExist: true, force:false, }, (err)=>{
+        if (err) {
+            throw new Error('FS operation failed')
+        }
+    } )
+    
 };
 
 await copy();
